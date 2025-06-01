28eac3c5f5eb8c39fac3be3b87f835a8adac4f1d
RogeriaLeandro <rogeriaaraujoleandro@gmail.com>
2025-06-01 12:44:33 -0300

    Criaçao do projeto

--- src/main/java/br/mil/defesa/interc2/Interc2GatewayEntradaAPI.java
+++ src/main/java/br/mil/defesa/interc2/Interc2GatewayEntradaAPI.java
@@ -1,7 +1,10 @@
 package br.mil.defesa.interc2;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Interc2GatewayEntradaAPI {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
     }
 }
